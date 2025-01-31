b7d95533eddaca115b1a985a1c3604c040e44d29
mercurioregon <jamesm620@gmail.com>
2025-01-31 13:49:54 -0800

    setup

---
+++ main.sql/nflmaster_db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE nflmaster_db;
+
+-- Create two new databases --
+DROP DATABASE IF EXISTS nflmaster_db;
+CREATE DATABASE nflmaster_db;
+
+-- Use nflmaster_db --
+USE nflmaster_db;
+
+-- See database in use --
+SELECT DATABASE();
